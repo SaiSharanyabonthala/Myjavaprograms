63a2972f2e99d26c520202a7b43ee278d892fd05
SaiSharanyabonthala <saisharanyabonthala@gmail.com>
2026-03-21 07:50:32 +0530

    Create weekday.java

---
+++ Basic projects/weekday.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class weekday{
+public static void main(String[] args){
+
+String day="honey";
+
+if (day.equals("Monday")){
+System.out.println("It is a weekday");
+}
+else if(day.equals("Tuesday")){
+System.out.println("It is a weekday");
+}
+else if(day.equals("Wednesday")){
+System.out.println("It is a weekday");
+}
+else if(day.equals("Thursday")){
+System.out.println("It is a weekday");
+}
+else if(day.equals("Friday")){
+System.out.println("It is a weekday");
+}
+else if(day.equals("Saturday")){
+System.out.println("It is a weekend");
+}
+else if(day.equals("Sunday")){
+System.out.println("It is a weekend");
+}
+else{
+System.out.println(day +" is not valid");
+}
+
+}}
+
+
+
